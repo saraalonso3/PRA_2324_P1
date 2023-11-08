9d5f45d8cc3430114690353143ec25e8282bdc3c
saraalonso3 <salonsop302@gmail.com>
2023-11-08 23:26:49 +0100

    he modificado el fichero de la clase drawing

--- Drawing.cpp
+++ Drawing.cpp
@@ -1,23 +1,58 @@
-#include "Drawing.h"
 #include "ListArray.h"
+#ifndef DRAWING_H
+#define DRAWING_H
 
-Drawing::Drawing(){
-	shapes = new ListArray<Shape*>
-}
+class Drawing {
+        private:
+                List<Shape*>* shapes;
+        public:
+		Drawing(){
+			shapes = new ListArray<Shape*>();
+		}
                 
-Drawing::~Drawing(){
-	for (int i = 0; i < n; i++) {
-            delete shapes[i];
-         
-        }
-}
+		~Drawing(){
+			for(int i = 0; i < shapes->n; i++){
+				delete shapes[i];
+			}
+		}
                 
-void Drawing::add_front(Shape* shape);
+		void add_front(Shape* shape){
+			shapes->prepend(shape); //utilizamos el metodo prepend de ListArray
+		}
                 
-void Drawing::add_back(Shape* shape);
+		void add_back(Shape* shape){
+			shapes->apend(shape); //utilizamos el método apend de ListArray
+		}
                 
-void Drawing::print_all();
+		void print_all(){
+			for(int i = 0; i < shapes->n; i++){
+				Shape* figura = shapes->get(i); // es la figura que se encuentra en la pos i
+				figura->print(); //accedemos al metodo print de Shape
+			}
+		}
 
-double Drawing::get_area_all_circles();
+		double get_area_all_circles(){
+			double area = 0;
+			for(int i = 0; i < shapes->n; i++){
+				Shape* figura = shapes->get(i);
+				Circle* circulo = dynamic_cast<Circle*>(figura); // conviertes figura en circulo
+				if(circulo){ // si se trata de un circulo
+					area = area + circulo->area(); //sumamos el area de todos los circulos
+				}
+			}
+			return area;
+		}
 
-void move_squares(double incX, double incY);
+		void move_squares(double incX, double incY){
+			for(int i = 0; i < shapes->n; i++){
+                	Shape* figura = shapes->get(i);
+                	Square* cuadrado = dynamic_cast<Square*>(figura); // conviertes figura en cuadrado
+                	if(cuadrado){
+				cuadrado->translate(incX, incY);
+			}
+		}
+		}
+
+};
+
+#endif
